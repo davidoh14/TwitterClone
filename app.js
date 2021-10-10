3e7426e36f8986e17e6b7fb98e74f26a6aec0edd
David Oh <davidohyoung@gmail.com>
2021-10-10 16:56:15 -0500

    auth setup

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const mongoose = require("mongoose");
-
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require('body-parser')
+const passport = require('passport');
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch((err) => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("changes"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
